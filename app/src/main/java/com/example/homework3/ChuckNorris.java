4c6992197d23e215d3ae5b2bbe2d1c99872e1ce5
suzytang <57127598+suzytang@users.noreply.github.com>
2020-04-14 12:09:26 +1000

    1st commit - functionalities fulfilled

--- app/src/main/java/com/example/homework3/ChuckNorris.java
+++ app/src/main/java/com/example/homework3/ChuckNorris.java
@@ -1,5 +1,6 @@
 package com.example.homework3;
 
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 
 import org.json.JSONException;
@@ -21,29 +22,23 @@ public class ChuckNorris extends AsyncTask<Void, Void, Void> {
     //background thread
     protected Void doInBackground(Void... voids) {
         try {
-            //import random quote Chuck Norris API url
-            URL url = new URL("https://api.chucknorris.io/jokes/random?category=dev,career");
+            //import random quote Chuck Norris API url. url filtered for development category
+            URL url = new URL("https://api.chucknorris.io/jokes/random?category=dev");
             //create http connection by opening a connection to the chuck norris url address
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             //create input stream to allow data to be read
             InputStream inputStream = httpURLConnection.getInputStream();
             //buffer reader to read data from inputStream
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            /*String line = "";
-            //while loop to read every line of the json file
-            while(line != null){
-                line = bufferedReader.readLine();
-                data = data + line;
-            }*/
             //bufferedReader reads the generated line of the json file
             data = bufferedReader.readLine();
 
             //create JSONObject and add every line of the json file to it
             JSONObject jsonObject = new JSONObject(data);
-            //parse value field (quote field) into the JSONObject
+            //parse value field from the API (the quote) into the JSONObject
             value = ""+ jsonObject.get("value");
 
-
+        //catch all exceptions from the try statement
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -59,8 +54,10 @@ public class ChuckNorris extends AsyncTask<Void, Void, Void> {
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
 
-        //set text of quote TextView to the "value" field of the json file
-        MainActivity.quote.setText(this.value);
+        //set text of quote TextView to the "value" field of the json file and add quotation marks around the value
+        MainActivity.quote.setText("''" + this.value + "''");
+        //set quote text to italics
+        MainActivity.quote.setTypeface(null, Typeface.ITALIC);
 
     }
 }
--- app/src/main/java/com/example/homework3/MainActivity.java
+++ app/src/main/java/com/example/homework3/MainActivity.java
@@ -1,34 +1,66 @@
 package com.example.homework3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.varunest.sparkbutton.SparkButton;
+import com.varunest.sparkbutton.SparkEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+
     public static TextView quote;
+    private SparkButton sparkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        quote = (TextView) findViewById(R.id.quote);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.title_bar);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        //intialise views
+        quote = (TextView) findViewById(R.id.quote);
+        sparkButton = (SparkButton) findViewById(R.id.sparkButton);
+
+        //sparkbutton library by varunes
+        //https://github.com/varunest/SparkButton
+        //listener for sparkbutton
+        sparkButton.setEventListener (new SparkEventListener() {
+            //calls ChuckNorris class on click of the sparkbutton and executes
             @Override
-            public void onClick(View v) {
+            public void onEvent(ImageView button, boolean buttonState) {
                 ChuckNorris chuckNorris = new ChuckNorris();
                 chuckNorris.execute();
+
             }
+
+            //did not use the following events from the library
+            @Override
+            public void onEventAnimationEnd(ImageView button, boolean buttonState) {
+                ;
+            }
+
+            @Override
+            public void onEventAnimationStart(ImageView button, boolean buttonState) {
+                ;
+            }
+
+
         });
 
     }
+
 }
